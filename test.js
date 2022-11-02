38e0ae853bb69ef3dcc25e364fed9544fef620d6
globalworming <andreas.worm@headissue.com>
2022-11-02 12:44:56 +0100

    DOM

--- test.js
+++ test.js
@@ -1,19 +1,45 @@
+function addSuccess(s) {
+    const item = document.createElement("li")
+    item.textContent = "✅ " + s
+    document.getElementById("tests").append(item)
+}
+
 const RunTest = {
     test1() {
-        setTimeout(() => {
-            try {
-                const unchecked = ["plant", "water", "wait", "harvest"].filter(it => {
-                    return document.getElementById(it).textContent !== "✅"
-                })
-                if (unchecked.length > 0) {
-                    document.getElementsByClassName("error")[0].textContent = `'${unchecked[0]}' is unchecked still`
-                    return
+
+        try {
+            const switchButton = () => document.querySelectorAll("#app button")[0];
+
+            (function appInitialStateShouldShowButtonText(t) {
+                const buttonText = switchButton().textContent
+                if (buttonText !== t) {
+                    throw new Error("expecting button to say 'heroes'")
                 }
-                document.getElementById("displayOnSuccess").hidden = false
-            } catch (e) {
-                document.getElementsByClassName("error")[0].textContent = e.message + "\n\n" + e.stack
-                throw e
-            }
-        }, 1000)
+                addSuccess("appInitialStateShouldShowButtonText")
+            })("heroes");
+
+
+            (function appInitialStateShouldShowTwoEntries() {
+                let numberOfListItems = document.querySelectorAll("#app li").length;
+                if (numberOfListItems !== 2) {
+                    throw new Error("expecting two list entries but was " + numberOfListItems)
+                }
+                addSuccess("appInitialStateShouldShowTwoEntries")
+
+            })();
+
+            (function removingAnEntryDecreasesListSize() {
+                let initialNumberOfListItems = document.querySelectorAll("#app li").length;
+                document.querySelectorAll("#app li button")[0].click()
+                let newNumberOfListItems = document.querySelectorAll("#app li").length;
+                if (newNumberOfListItems !== initialNumberOfListItems - 1) {
+                    throw new Error("expecting one less entries")
+                }
+                addSuccess("removingAnEntryDecreasesListSize")
+            })();
+        } catch (e) {
+            document.getElementsByClassName("error")[0].textContent = e.message + "\n\n" + e.stack
+            throw e
+        }
     }
 }
